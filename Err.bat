9c767fd180ec1e5e0d46e533b6e666fe78ccd7b4
MichelZaal85 <michel@my-mirror.nl>
2016-01-04 19:06:22 +0100

    line 37 ERROR - .. -> ERROR -- ...

---
+++ Err.bat
@@ -0,0 +1,64 @@
+﻿@echo off
+prompt $g
+cls
+@echo off
+:loop
+@echo off
+ping -n 2 127.0.0.1>nul
+@echo off
+echo VIRUS DETECTED!
+@echo off
+ping -n 2 127.0.0.1>nul
+@echo off
+ping -n 2 127.0.0.1>nul
+@echo off
+ping -n 2 127.0.0.1>nul
+@echo off
+cls
+echo WINDOW DEFENDER ACTIVE
+ping -n 2 127.0.0.1>nul
+@echo off
+echo .
+ping -n 2 127.0.0.1>nul
+@echo off
+echo .
+ping -n 2 127.0.0.1>nul
+@echo off
+echo .
+ping -n 2 127.0.0.1>nul
+@echo off
+echo .
+ping -n 2 127.0.0.1>nul
+@echo off
+ping -n 2 127.0.0.1>nul
+@echo off
+ping -n 2 127.0.0.1>nul
+@echo off
+echo ERROR! -- PC HACKED
+@echo off
+ping -n 2 127.0.0.1>nul
+ping -n 2 127.0.0.1>nul
+@echo off
+cls
+@echo off
+ipconfig
+@echo off
+ping 127.0.0.1 -n 7 -w 1000
+@echo off
+cls
+echo Initiate File Extraction....
+@echo off
+ping -n 2 127.0.0.1>nul
+@echo off
+ping -n 2 127.0.0.1>nul
+@echo off
+ping -n 2 127.0.0.1>nul
+@echo off
+cd c:\
+dir /w/s
+@echo off
+cls
+@echo off 
+echo System failure!
+pause
+Exit
